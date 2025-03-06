77e93f62b238472b34556aa0d54571049cde01e3
SuThetHnin03 <suthethnin24603@gmail.com>
2025-03-06 11:03:59 +0630

    API integration

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
-function generatePoem(event){
-    event.preventDefault();
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
-    new Typewriter("#poem", {
-        strings: "La tombe dit à la rose",
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-      });
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructions = document.querySelector("#instructions")
+  let apiKey = "0a3a6729o75585fadatfdbb88a4a9a1a";
+  let prompt = `Generate a poems which are related to ${instructions.value}`;
+  let context = "Give a meaninful and also romantic one seperated in lines using <br> in english. Include the writers and country of origin below the poem like Jon Doe (America). Do not include title. If aurthor is unknown add Anonymous and if there is two aurthors connect with &";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem)
 }
 
 let poemInput = document.querySelector("#poem-input");
