4de6c43b6a050abc4f23ab3857ba6d194d2b2b1b
SuThetHnin03 <suthethnin24603@gmail.com>
2025-03-06 17:04:00 +0630

    adding user experience

--- src/app.js
+++ src/app.js
@@ -13,8 +13,11 @@ function generatePoem(event) {
   let instructions = document.querySelector("#instructions")
   let apiKey = "0a3a6729o75585fadatfdbb88a4a9a1a";
   let prompt = `Generate a poems which are related to ${instructions.value}`;
-  let context = "Give a meaninful and also romantic one seperated in lines using <br> in english. Include the writers and country of origin below the poem like Jon Doe (America). Do not include title. If aurthor is unknown add Anonymous and if there is two aurthors connect with &";
+  let context = "Give a meaninful and also romantic one seperated in lines using <br> in english. Include the writers and country of origin below the poem. Do not include title. If aurthor is unknown add Anonymous and if there is two aurthors connect with &";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  poem.classList.remove("hidden");
+  poem.innerHTML = `<div class="generating">⏳ </div> Generating a French poem about ${instructions.value}`;
 
   axios.get(apiUrl).then(displayPoem)
 }
