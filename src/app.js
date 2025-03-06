a374bd377ee03014d269ad8e227a2111835432bb
SuThetHnin03 <suthethnin24603@gmail.com>
2025-03-06 09:31:30 +0630

    add basic js and basic typewrite effect

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log("Hwllo World");
+function generatePoem(event){
+    event.preventDefault();
+
+    new Typewriter("#poem", {
+        strings: "La tombe dit à la rose",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+}
+
+let poemInput = document.querySelector("#poem-input");
+let poem = document.querySelector("#poem")
+
+poemInput.addEventListener("submit", generatePoem);
